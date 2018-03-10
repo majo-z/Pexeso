cad532c7ade86fc93431cd221b02384c08541433
majo-z <g00340481@gmit.ie>
2018-03-10 20:04:47 +0000

    displaying randomly selected images from the images folder

--- Pexeso/MainPage.xaml.cs
+++ Pexeso/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 
@@ -24,11 +25,13 @@ namespace Pexeso
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Random _rnd = new Random();
         public MainPage()
         {
             this.InitializeComponent();
             InitGrid(8);
-            GeneratePossiblePositions(8);
+            FillPositions();
+            // GeneratePossiblePositions(8);
         }
 
         //crate and draw rectangles
@@ -36,30 +39,40 @@ namespace Pexeso
         {
             for (int i = 0; i < size; i++)
             {
-                GameGrid.RowDefinitions.Add(new RowDefinition());
-                GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                gameGrid.RowDefinitions.Add(new RowDefinition());
+                gameGrid.ColumnDefinitions.Add(new ColumnDefinition());
             }
-
+            /*
             for (int row = 0; row < size; row++)
             {
                 for (int col = 0; col < size; col++)
                 {
                     Rectangle rect = new Rectangle();
-                    rect.Fill = new SolidColorBrush(Colors.Red);
+
+                    // imagebrush is an object
+                    ImageBrush brush = new ImageBrush();
+                    Uri uri = new Uri("ms-appx:///Images/1.png", UriKind.RelativeOrAbsolute);
+                    BitmapImage bitmap = new BitmapImage(uri);
+
+                    brush.ImageSource = bitmap;
+
+                    rect.Fill = brush;
+                    //rect.Fill = new SolidColorBrush(Colors.Red);
                     rect.SetValue(Grid.RowProperty, row);
                     rect.SetValue(Grid.ColumnProperty, col);
-                    rect.Width = 50;
-                    rect.Height = 50;
-                    GameGrid.Children.Add(rect);
+                    rect.Width = 20;
+                    rect.Height = 20;
+                    gameGrid.Children.Add(rect);
                 }
 
             }
+            */
         }
 
         // create set of all possible positions
         private List<string> GeneratePossiblePositions(int size)
         {
-            //HashSet<string> set = new HashSet<string>();
+
             var list = new List<string>();
 
             for (var row = 0; row < size; row++)
@@ -81,11 +94,84 @@ namespace Pexeso
 
         }
 
-        // loop through each one
-        // take two off the top
-        // create 2 rectanges
-        // assign same id to both
-        // place them on the grid
+        private Rectangle GenerateRectangle(int imageNum, string tag)
+        {
+            Rectangle rect = new Rectangle();
+            ImageBrush brush = new ImageBrush(); // imagebrush is an object
+            Uri uri = new Uri("ms-appx:///Images/" + imageNum.ToString() + ".png", UriKind.RelativeOrAbsolute);
+            BitmapImage bitmap = new BitmapImage(uri);
+
+            brush.ImageSource = bitmap;
+
+            rect.Fill = brush;
+            rect.Tag = tag; // assign id as the tag
+
+            return rect;
+        }
+
+        private void AddRectangleToGrid(Rectangle rect, int row, int col)
+        {
+            rect.SetValue(Grid.RowProperty, row);
+            rect.SetValue(Grid.ColumnProperty, col);
+            rect.Width = 20;
+            rect.Height = 20;
+            gameGrid.Children.Add(rect);
+        }
+
+        private void FillPositions()
+        {
+            // get all possible positions
+            var allPositions = new Stack<string>(GeneratePossiblePositions(8));
+            while (allPositions.Count > 0) // keep going until no positions are left
+            {
+              
+                
+
+                // https://stackoverflow.com/questions/2706500/how-do-i-generate-a-random-int-number-in-c
+                
+                int imageNum = _rnd.Next(1, 4);   // creates a number between 1 and 3
+
+                // create 2 rectanges and connect them
+                var rect1 = GenerateRectangle(imageNum, "?");
+
+                var rect2 = GenerateRectangle(imageNum, "?");
+
+                // take two off the top
+                string first = allPositions.Pop(); // "0_0"
+                string second = allPositions.Pop(); // "1_6" 
+
+                // https://stackoverflow.com/questions/8928601/how-can-i-split-a-string-with-a-string-delimiter
+                var row1Col1 = first.Split('_');//string[] row1Col1 = first.Split('_');
+                var row2Col2 = second.Split('_'); // string[] ro2Col2 = second.Split('_');
+
+                // https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
+                int row1 = Int32.Parse(row1Col1[0]);
+                int col1 = Int32.Parse(row1Col1[1]);
+                int row2 = Int32.Parse(row2Col2[0]);
+                int col2 = Int32.Parse(row2Col2[1]);
+
+
+                AddRectangleToGrid(rect1, row1, col1);
+                AddRectangleToGrid(rect2, row2, col2);
+                // place them on the grid
+
+            }
+
+        }
+       
+        
+        
+        
+        
+
+        //game reset button
+        private void gameReset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+       
 
     }
 }
